478b2047a97b7a6b8fa86f6e56eb74cebcfd2b54
Siri-101 <iyeresiri.ogbe@pau.edu.ng>
2025-10-15 15:35:23 +0100

    practice 8

---
+++ week-3/practice_8/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let mut fees = 25_000;
+    println!("Fees: {}", fees);
+
+    fees = 35_000;
+    println!("Fees is changed to: {}", fees)
+}
---
+++ week-3/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\New\Downloads\i.ogbeSEN101\week-3\practice_8\target\debug\practice_8.exe: C:\Users\New\Downloads\i.ogbeSEN101\week-3\practice_8\src\main.rs
