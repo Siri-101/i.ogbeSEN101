a35af66cd87e5f3b2762e89daa61e45e42dcd0fe
Siri-101 <iyeresiri.ogbe@pau.edu.ng>
2025-10-15 15:49:51 +0100

    practice 4 variable naming correction

--- week-3/practice_4/src/main.rs
+++ week-3/practice_4/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
-    let isgood:bool = true;
-    println!("PAU is a good University: {}", isgood);
+    let is_good:bool = true;
+    println!("PAU is a good University: {}", is_good);
 }
