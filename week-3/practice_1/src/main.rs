dedb6ccaea4405adac63e96b7404242f64d6a92a
Siri-101 <iyeresiri.ogbe@pau.edu.ng>
2025-10-15 15:14:55 +0100

    m

---
+++ week-3/practice_1/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let school_string = "School Of Science and Technology";
+    let rating_float = 5.0;
+    let is_growing = true;
+    let department_int = 3;
+
+
+    println!("School Name: {}", school_string);
+    println!("School Rating: {}", rating_float);
+    println!("School is growing: {}", is_growing);
+    println!("Number of Departments: {}", department_int);
+}
