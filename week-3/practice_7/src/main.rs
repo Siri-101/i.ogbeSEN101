ed83c88eb26f070a5282136e139433c4d2700450
Siri-101 <iyeresiri.ogbe@pau.edu.ng>
2025-10-15 15:33:34 +0100

    practice 7

---
+++ week-3/practice_7/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let float_with_separator = 11_000.555_000;
+    println!("Float: {}", float_with_separator);
+
+    let int_with_separator = 50_000;
+    println!("Integer: {}", int_with_separator);
+}
