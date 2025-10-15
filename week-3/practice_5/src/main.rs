5a41d9ac08c2bf0ab9a1f053ccc1cc907cfab312
Siri-101 <iyeresiri.ogbe@pau.edu.ng>
2025-10-15 15:29:22 +0100

    practice 5

---
+++ week-3/practice_5/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let special_character = '@';
+    let alphabet:char = 'B';
+    let surname = "Bolaji";
+    let firstname = "Michael";
+    let middle_name = "Miles";
+
+
+    println!("");
+    println!("Special Character: {}", special_character);
+    println!("Alphabet: {}", alphabet);
+    println!("0Name: {} {} {}", surname, middle_name, firstname);
+
+}
