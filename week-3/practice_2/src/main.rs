02fc6e2e52363e0d9fe86755833750e049562c80
Siri-101 <iyeresiri.ogbe@pau.edu.ng>
2025-10-15 15:17:38 +0100

    practice 2

---
+++ week-3/practice_2/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let result = 10;
+    let age:u32 = 20;
+    let sum:i32 = 5 - 15;
+
+
+    println!("Result: {}", result);
+    println!("Age: {}", age);
+    println!("Sum: {}", sum);
+}
