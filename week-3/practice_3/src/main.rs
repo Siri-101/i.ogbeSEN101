056d30fcb933d9c4a9dd91d67547fad9e72906ba
Siri-101 <iyeresiri.ogbe@pau.edu.ng>
2025-10-15 15:20:32 +0100

    practice 3

---
+++ week-3/practice_3/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let result = 10.00;
+    let interest:f32 = 8.35;
+    let sum:f32 = 15000.600;
+
+
+    println!("Result: {}", result);
+    println!("Interest: {}", interest);
+    println!("Sum: {}", sum);
+}
