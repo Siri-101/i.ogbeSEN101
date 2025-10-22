df84f536a3eb51e2fed000fde41d0ec0115c05d8
Siri-101 <iyeresiri.ogbe@pau.edu.ng>
2025-10-22 19:39:44 +0100

    Project 2 Commit

---
+++ week-4/project_2/src/main.rs
@@ -0,0 +1,32 @@
+use std::io;
+
+fn main() {
+    let mut is_experienced = String::new();
+
+    println!("Is the Employee Experienced (Y/N): ");
+    io::stdin().read_line(&mut is_experienced).expect("Could not Read Input");
+    if is_experienced.trim() == "Y" {
+        let mut input2 = String::new();
+        println!("Age: ");
+        io::stdin().read_line(&mut input2).expect("Could not Read Input");
+        let age:u16 = input2.trim().parse().expect("Is not an Integer");
+
+        if age >= 40 {
+            println!("Incentive: N1,560,000");
+        }
+        else if age >= 30 && age < 40 {
+            println!("Incentive: N1,480,000");
+        }
+        else {
+            println!("Incentive: N1,300,000");
+        }
+
+    }
+    else {
+        println!("Incentive: N100,000");
+    }
+
+    // let mut age = String::new();
+
+    // println!("Age: ")
+}
